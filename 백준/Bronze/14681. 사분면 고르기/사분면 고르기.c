433d92dae86cb4d11181b100f3687933a8bf5b2f
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-03-06 08:13:58 +0900

    [Bronze V] Title: 사분면 고르기, Time: 0 ms, Memory: 1116 KB -BaekjoonHub

---
+++ 백준/Bronze/14681. 사분면 고르기/사분면 고르기.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main(){
+    int x,y;
+    scanf("%d %d",&x,&y);
+    if(x>0){
+        if(y>0){
+            printf("1");
+        }else{
+            printf("4");
+        }
+    }else{
+        if(y>0){
+            printf("2");
+        }else{
+            printf("3");
+        }
+    }
+    return 0;
+}
