d390a4912b7df7fcc6cfc57512c06c584c2d58e1
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-03-12 08:31:48 +0900

    [Bronze II] Title: 수 정렬하기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

---
+++ 백준/Bronze/2750. 수 정렬하기/수 정렬하기.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <string.h>
+int main(void){
+    int i,j,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(arr[i]<arr[j]){
+                int temp = arr[i];
+                arr[i] = arr[j];
+                arr[j] = temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
