495607c1634b98988e20535809c438f69cd0fd76
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-03-12 08:40:17 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 536 ms, Memory: 8808 KB -BaekjoonHub

---
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int static compare (const void* first, const void* second)
+{
+    if (*(int*)first > *(int*)second)
+        return 1;
+    else if (*(int*)first < *(int*)second)
+        return -1;
+    else
+        return 0;
+}
+int main(void){
+    int i,n;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++){
+        scanf("%d",&arr[i]);
+    }
+    qsort(arr,n, sizeof(int),compare);
+    for(i=0;i<n;i++){
+        printf("%d\n",arr[i]);
+    }
+    return 0;
+}
