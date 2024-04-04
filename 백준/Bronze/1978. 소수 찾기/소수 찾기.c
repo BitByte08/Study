a22733694649938e972a4d7f3a330237abae6ef9
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-04-04 20:03:23 +0900

    [Bronze II] Title: 소수 찾기, Time: 0 ms, Memory: 1112 KB -BaekjoonHub

--- 백준/Bronze/1978. 소수 찾기/소수 찾기.c
+++ 백준/Bronze/1978. 소수 찾기/소수 찾기.c
@@ -1,27 +1,21 @@
 #include <stdio.h>
-#include <stdlib.h>
-/*
- 첫 줄에 수의 개수 N이 주어진다. N은 100이하이다. 다음으로 N개의 수가 주어지는데 수는 1,000 이하의 자연수이다.
- */
-int main(void){
-    int n,max,i,j,cnt,cnt_2;
-    max=n=i=j=cnt=cnt_2=0;
-    scanf("%d",&max);
-    for(i=0;i<max;i++){
-        cnt=0;
-        scanf("%d",&n);
-        if(n==1){
-            continue;
-        }
-        for(j=1;j<=n;j++){
-            if(n%j==0){
-                cnt++;
-            }
-        }
-        if(cnt==2){
-            cnt_2++;
+
+int prime(int n){
+    if(n<2)return 0;
+    for(int i=2;i*i<=n;i++){
+        if(n%i==0){
+            return 0;
         }
     }
-    printf("%d",cnt_2);
+    return 1;
+}
+int main(void){
+    int t,n,cnt=0;
+    scanf("%d",&t);
+    for(int i=0;i<t;i++){
+        scanf("%d",&n);
+        if(prime(n)) cnt++;
+    }
+    printf("%d",cnt);
     return 0;
 }
