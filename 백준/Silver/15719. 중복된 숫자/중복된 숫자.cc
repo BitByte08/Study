88d5ecd035a9a60ed69e2d28cdf45d8507697063
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-05-03 00:40:44 +0900

    [Silver V] Title: 중복된 숫자, Time: 1052 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
+++ 백준/Silver/15719. 중복된 숫자/중복된 숫자.cc
@@ -6,18 +6,13 @@ int main(void){
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
     cout.tie(NULL);
-    int n;
+    unsigned long long int n,i,a,cnt=0;
     cin >> n;
-    int arr[9999999]={};
-    for(int i=0;i<n;i++){
-        int a;
+    unsigned long long int num1 = (n-1)*n/2;
+    for(i=0;i<n;i++){
         cin >> a;
-        if(arr[a]==1){
-            cout << a;
-            break;
-        }
-        arr[a] = 1;
-        
+        cnt+=a;
     }
+    cout << cnt-num1;
     return 0;
 }
