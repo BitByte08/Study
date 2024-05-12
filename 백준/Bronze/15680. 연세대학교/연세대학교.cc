2c687bc503e962d376e9547f83136df0e58aa331
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-05-12 21:46:34 +0900

    [Bronze V] Title: 연세대학교, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    int n;
+    cin >> n;
+    if(n){
+        cout << "Leading the Way to the Future";
+    }else{
+        cout << "YONSEI";
+    }
+    return 0;
+}
