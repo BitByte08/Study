2669b20cee2ec264729fb69a3e0c7943a4088790
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-05-19 23:42:21 +0900

    [Bronze III] Title: 핸드폰 요금, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
+++ 백준/Bronze/1267. 핸드폰 요금/핸드폰 요금.cc
@@ -3,24 +3,24 @@
 using namespace std;
 
 int main(void) {
-    int N, Y = 0, M = 0;
-    
-    cin >> N;
+    int t,Y,M;
+    Y=M=0;
+    cin >> t;
     int i;
-    for (i = 0; i < N; i++) {
-        int t;
-        cin >> t;
-        Y += (t / 30 + 1) * 10;
-        M += (t / 60 + 1) * 15;
+    for (i=0;i<t;i++) {
+        int temp;
+        cin >> temp;
+        Y+=(temp/30+1);
+        M+=(temp/60+1);
     }
-
-    if (Y == M) {
-        cout << "Y M " << Y << "\n";
-    } else if (Y > M){
-        cout << "M " << M << "\n";
+    Y*=10;
+    M*=15;
+    if(Y==M){
+        cout << "Y M " << Y;
+    }else if(Y>M){
+        cout << "M " << M;
     }else{
         cout << "Y " << Y << "\n";
     }
-    
     return 0;
 }
