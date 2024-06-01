e2f85221033638e35d710bbed5e069f677ab2fec
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-06-01 19:52:25 +0900

    [Bronze IV] Title: 인공지능 시계, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/2530. 인공지능 시계/인공지능 시계.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void) {
+    int h,m,s,t;
+    cin >> h >> m >> s >> t;
+    s+=t;
+    m+=s/60;
+    s%=60;
+    h+=m/60;
+    m%=60;
+    h%=24;
+    cout << h << ' ' << m << ' ' << s;
+    return 0;
+}
