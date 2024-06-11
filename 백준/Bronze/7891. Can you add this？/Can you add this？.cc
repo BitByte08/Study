cc180b5f9ceec21b3d61b3fe9d8bd14af0ef284b
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-06-11 16:19:52 +0900

    [Bronze V] Title: Can you add this?, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7891. Can you add this？/Can you add this？.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+
+using namespace std;
+
+int main(void){
+    long long a,b;
+    int n;
+    cin >> n;
+    while(n--){
+        cin >> a >> b;
+        cout << a+b << '\n';
+    }
+    return 0;
+}
