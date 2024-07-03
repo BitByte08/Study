4855694a96275c3e72a655897401dfafb5f7b3cc
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-07-03 21:29:24 +0900

    [Silver II] Title: 최대 힙, Time: 12 ms, Memory: 2916 KB -BaekjoonHub

---
+++ 백준/Silver/11279. 최대 힙/최대 힙.cc
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    priority_queue<int> pq;
+    int t; cin >> t;
+    while(t--){
+        int temp; cin >> temp;
+        if(temp==0) {
+            if(pq.empty()){
+                cout << 0 << '\n';
+                continue;
+            }
+            cout << pq.top() << '\n';
+            pq.pop();
+            continue;
+        }
+        pq.push(temp);
+    }
+    return 0;
+}
