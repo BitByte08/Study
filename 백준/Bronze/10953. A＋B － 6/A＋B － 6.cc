5fa3e073f3df71ee09e1387e91ce3017528716c7
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-07-07 21:50:38 +0900

    [Bronze III] Title: A+B - 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10953. A＋B － 6/A＋B － 6.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+#include <cstdio>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while(t--){
+        int a,b;scanf("%d,%d",&a,&b);
+        cout << a+b << '\n';
+    }
+    return 0;
+}
