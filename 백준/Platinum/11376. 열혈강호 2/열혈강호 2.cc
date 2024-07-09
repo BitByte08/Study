69f634b4878cb6236bfe4b2a04a40d76c4ae734c
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-07-09 09:35:09 +0900

    [Platinum IV] Title: 열혈강호 2, Time: 616 ms, Memory: 6140 KB -BaekjoonHub

---
+++ 백준/Platinum/11376. 열혈강호 2/열혈강호 2.cc
@@ -0,0 +1,44 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <string.h>
+
+using namespace std;
+
+vector<int> v[1001];
+bool visited[1001];
+int work[1001];
+
+bool dfs(int n){
+    if(visited[n]) return false;
+    visited[n]=true;
+    for(auto next:v[n]){
+        if(!work[next]||dfs(work[next])){
+            work[next] = n;
+            return true;
+        }
+    }
+    return false;
+}
+int main(void){
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int n,m; cin >> n >> m;
+    for(int i=1;i<=n;i++){
+        int o; cin >> o;
+        for(int j=0;j<o;j++){
+            int temp; cin >> temp;
+            v[i].push_back(temp);
+        }
+    }
+    int res=0;
+    for(int i=1; i<=n;i++){
+        for(int j=0;j<2;j++) {
+            memset(visited, false, sizeof(visited));
+            if (dfs(i))res++;
+        }
+    }
+    cout << res;
+    return 0;
+}
