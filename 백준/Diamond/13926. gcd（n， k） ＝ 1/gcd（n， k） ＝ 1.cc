7b904330cdb99ae49f24d93ed9c6b1f66fe3dd60
BitByte08 <153086880+BitByte08@users.noreply.github.com>
2024-07-25 20:35:47 +0900

    [Diamond V] Title: gcd(n, k) = 1, Time: 20 ms, Memory: 2028 KB -BaekjoonHub

---
+++ 백준/Diamond/13926. gcd（n， k） ＝ 1/gcd（n， k） ＝ 1.cc
@@ -0,0 +1,90 @@
+#include <bits/stdc++.h>
+#define ull unsigned long long
+using namespace std;
+
+ull Power(__int128 x, __int128 y, __int128 mod) { // ret = (x^y)%mod
+    x %= mod;
+    __int128 ret = 1;
+    while(y > 0) {
+        if(y%2 == 1) ret = (ret*x)%mod;
+        x = (x*x)%mod;
+        y /= 2;
+    }
+    return (ull)ret;
+}
+
+bool checkPrime(ull n, ull a) {
+    if(a%n == 0) return true;
+    ull k = n-1;
+    while(1) {
+        ull temp = Power(a, k, n);
+        if(temp == n-1) return true;
+        if(k%2 == 1) return (temp == 1 || temp == n-1);
+        k /= 2;
+    }
+}
+
+bool isPrime(ull n) {
+    if(n == 2 || n == 3) return true;
+    if(n%2 == 0) return false;
+
+    ull prime[] = {2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37};
+    for(int i=0; i<12; i++)
+        if(!checkPrime(n, prime[i])) {
+            return false;
+            break;
+        }
+    return true;
+}
+
+ull gcd(ull a, ull b) {
+    if(a < b) swap(a, b);
+    while(b != 0) {
+        ull r = a%b;
+        a = b;
+        b = r;
+    }
+    return a;
+}
+
+ull findnum(__int128 n) {
+    if(n%2 == 0) return 2;
+    if(isPrime(n)) return n;
+
+    __int128 x = rand()%(n-2) + 2, y = x, c = rand()%10 + 1, g = 1;
+    while(g == 1) {
+        x = (x*x%n + c)%n;
+        y = (y*y%n + c)%n;
+        y = (y*y%n + c)%n;
+        g = gcd(abs(x-y), n);
+        if(g == n) return findnum(n);
+    }
+    if(isPrime(g)) return g;
+    else return findnum(g);
+}
+
+int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL); cout.tie(NULL);
+
+    ull N;
+    cin >> N;
+    if(N==1) {
+        cout << 1;
+        return 0;
+    }
+    ull temp = N;
+    
+    vector<ull> res;
+    while(N > 1) {
+        ull num = findnum(N);
+        res.push_back(num);
+        N /= num;
+    }
+    sort(res.begin(), res.end());
+    ull ans = temp/res[0]*(res[0]-1);
+    for(int i=1;i<res.size();i++) {
+        if(res[i]!=res[i-1]) ans = ans/res[i]*(res[i]-1);
+    }
+    cout << ans;
+}
