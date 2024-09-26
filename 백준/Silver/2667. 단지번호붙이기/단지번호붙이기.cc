effd2befee81155a6a585b442036ed3906f5084b
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-09-26 20:31:09 +0900

    [Silver I] Title: 단지번호붙이기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
+++ 백준/Silver/2667. 단지번호붙이기/단지번호붙이기.cc
@@ -6,16 +6,15 @@ vector<vector<int>> visited(25,vector<int>(25,0));
 vector<vector<int>> v(25,vector<int>(25,0));
 vector<pair<int,int>> d{{1,0},{-1,0},{0,1},{0,-1}};
 int n;
-int cnt;
-void dfs(int x,int y) {
-  for(auto i:d) {
-    if(X>=n || X<0 || Y>=n || Y<0) continue;
-    if(!visited[X][Y] && v[X][Y]==1) {
-      visited[X][Y]=1;
-      cnt++;
-      dfs(X,Y);
-    }
-  }
+int dfs(int x,int y) {
+  int cnt=1;
+  visited[x][y]=1;
+  for(auto i:d)
+    if(X<n && X>=0 && Y<n && Y>=0)
+      if(!visited[X][Y] && v[X][Y]==1) {
+        cnt += dfs(X,Y);
+      }
+  return cnt;
 }
 
 int main(void) {
@@ -25,15 +24,10 @@ int main(void) {
     string s; cin >> s;
     for(int j=0;j<n;j++) v[i][j] = s[j]-'0';
   }
+  int cnt = 0;
   for(int i=0;i<n;i++) {
     for(int j=0;j<n;j++) {
-      if(v[i][j]&&!visited[i][j]){
-        visited[i][j] = 1;
-        cnt=1;
-        dfs(i,j);
-        res.push_back(cnt);
-
-      }
+      if(v[i][j]&&!visited[i][j])res.push_back(dfs(i,j));
     }
   }
   cout << res.size() << '\n';
