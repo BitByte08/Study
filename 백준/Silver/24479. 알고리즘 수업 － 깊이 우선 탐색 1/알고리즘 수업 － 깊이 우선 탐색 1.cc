bc3fe18a1483c57979719c01d31232131d09608c
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-10-04 22:12:15 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 1, Time: 180 ms, Memory: 12356 KB -BaekjoonHub

---
+++ 백준/Silver/24479. 알고리즘 수업 － 깊이 우선 탐색 1/알고리즘 수업 － 깊이 우선 탐색 1.cc
@@ -0,0 +1,28 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<vector<int>> v(100001,vector<int>(0));
+vector<int> visited(100001,0);
+int cnt=1;
+void dfs(int n) {
+  if (visited[n]) return;
+  visited[n] = cnt++;
+  sort(v[n].begin(),v[n].end());
+  for (auto i : v[n]) {
+    dfs(i);
+  }
+}
+int main() {
+  int n,m,r; cin>>n>>m>>r;
+  for (int i=0;i<m;i++) {
+    int x,y; cin>>x>>y;
+    v[x].push_back(y);
+    v[y].push_back(x);
+  }
+  dfs(r);
+  for (int i=1;i<=n;i++) {
+    cout<<visited[i]<<'\n';
+  }
+  return 0;
+}
