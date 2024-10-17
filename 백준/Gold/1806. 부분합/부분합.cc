a3c4a74c310a1ac330b67452c3c4042fce58e611
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-10-17 17:54:27 +0900

    [Gold IV] Title: 부분합, Time: 8 ms, Memory: 2412 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.cc
+++ 백준/Gold/1806. 부분합/부분합.cc
@@ -3,18 +3,18 @@
 using namespace std;
 vector<int> v;
 int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
   int N,S; cin >> N >> S;
+  int start=0,end=0,sum=0,ans=N+1;
   v.resize(N,0);
   for(int i=0;i<N;i++) cin >> v[i];
-  int start=0,end=0,sum=0,ans=N+1;
   while(1) {
     if(sum>=S) {
       ans=min(ans,end-start);
       sum-=v[start++];
-    }
-    else if(end==N) break;
+    }else if(end==N) break;
     else sum+=v[end++];
-  }
-  cout << (ans==N+1?0:ans);
+  }cout << (ans==N+1?0:ans);
   return 0;
 }
