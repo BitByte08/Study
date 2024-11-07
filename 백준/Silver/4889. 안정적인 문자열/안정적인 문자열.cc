db433390aa1471d652e1f24ce418db8ac5b5d365
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-11-08 08:35:32 +0900

    [Silver I] Title: 안정적인 문자열, Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/4889. 안정적인 문자열/안정적인 문자열.cc
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <string>
+#include <stack>
+using namespace std;
+int main() {
+  int t = 1;
+  while (1) {
+    string s; cin >> s;
+    if (s[0] == '-') break;
+    int result = 0;
+    stack<char> st;
+    for (int i = 0; i < s.size(); i++) {
+      char temp = s[i];
+      if (temp == '}' && !st.empty() && st.top() == '{') st.pop();
+      else st.push(temp);
+    }
+    while (!st.empty()) {
+      char temp1 = st.top(); st.pop();
+      char temp2 = st.top(); st.pop();
+      if (temp1 == temp2) result++;
+      else result += 2;
+    }
+    cout << t << ". " << result << '\n';
+    t++;
+  }
+  return 0;
+}
