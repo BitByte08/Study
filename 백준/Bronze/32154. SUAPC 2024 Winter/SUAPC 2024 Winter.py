af073a4bf2a1395bb4a9d1c31095b56ddf419aa9
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-11-15 22:33:30 +0900

    [Bronze V] Title: SUAPC 2024 Winter, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/32154. SUAPC 2024 Winter/SUAPC 2024 Winter.py
@@ -0,0 +1,31 @@
+N = int(input())
+if N == 1 : 
+    print(11)
+    print("A B C D E F G H J L M")
+elif N == 2 :
+    print(9)
+    print("A C E F G H I L M")
+elif N == 3 :
+    print(9)
+    print("A C E F G H I L M")
+elif N == 4 :
+    print(9)
+    print("A B C E F G H L M")
+elif N == 5 :
+    print(8)
+    print("A C E F G H L M")
+elif N == 6 :
+    print(8)
+    print("A C E F G H L M")
+elif N == 7 :
+    print(8)
+    print("A C E F G H L M")
+elif N == 8 :
+    print(8)
+    print("A C E F G H L M")
+elif N == 9 :
+    print(8)
+    print("A C E F G H L M")
+elif N == 10 :
+    print(8)
+    print("A B C F G H L M")
