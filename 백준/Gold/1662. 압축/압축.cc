655028f543e50282d7db8a6440f5deee53d60706
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-11-20 11:26:15 +0900

    [Gold IV] Title: 압축, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Gold/1662. 압축/압축.cc
+++ 백준/Gold/1662. 압축/압축.cc
@@ -1,26 +1,16 @@
 #include <iostream>
-#include <stack>
-#include <cctype>
-#define ADD res+=st.top(); st.pop();
 using namespace std;
-int main(){
-  stack<int> st;
-  string s; cin >> s;
+int decoder(string s, char c){
+  static int i = -1; i++;
   int res=0;
-  for(int i=0;i<s.size();i++){
-    if(s[i]=='(') st.push(-1);
-    else if(isdigit(s[i])){
-      if(s[i+1]=='(') st.push(s[i]-'0');
-      else st.push(1);
-    }else{
-      res=0;
-      while(st.top()!=-1) { ADD }
-      st.pop();
-      res*=st.top(); st.pop();
-      st.push(res);
-    }
+  while(s[i++]!=c) {
+    if(s[i]=='(') res+=atoi(&s[i-1])*decoder(s,')');
+    else res++;
   }
-  res=0;
-  while(!st.empty()){ ADD }
-  cout << res;
+  return res;
+}
+int main(void){
+  string s; cin >> s;
+  cout << decoder(s,0);
+  return 0;
 }
