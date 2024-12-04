6400ab733e775715152f6ac8ad51a40dcf77f567
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-12-04 21:59:36 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 120 ms, Memory: 4380 KB -BaekjoonHub

---
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -0,0 +1,43 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#define Pair pair<int,pair<int,int>>
+using namespace std;
+int v[10001];
+int find_parent(int x);
+void union_parent(int x,int y);
+int main(void) {
+  int n,m; cin >> n >> m;
+  vector<Pair> edges;
+  for (int i=1;i<=n;i++) {
+    v[i]=i;
+  }
+  for (int i=0;i<m;i++) {
+    int a,b,cost; cin >> a >> b >> cost;
+    edges.push_back({cost,{a,b}});
+  }
+  int res = 0;
+  sort(edges.begin(),edges.end());
+  for (Pair edge: edges) {
+    pair<int,int> cost = edge.second;
+    if (find_parent(cost.first)!=find_parent(cost.second)) {
+      union_parent(cost.first,cost.second);
+      res+=edge.first;
+    }
+  }
+  cout << res;
+  return 0;
+}
+int find_parent(int x) {
+  if (v[x]!=x)v[x]=find_parent(v[x]);
+  return v[x];
+}
+void union_parent(int x,int y) {
+  x = find_parent(x);
+  y = find_parent(y);
+  if (x<y) {
+    v[y]=x;
+  }else {
+    v[x]=y;
+  }
+}
