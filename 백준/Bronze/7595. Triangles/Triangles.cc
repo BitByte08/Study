fec759dac4e1cfa234f346d0075022dbef8270bf
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-12-08 10:50:57 +0900

    [Bronze IV] Title: Triangles, Time: 276 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/7595. Triangles/Triangles.cc
@@ -0,0 +1,14 @@
+#include <iostream>
+using namespace std;
+int main(void) {
+  while (true) {
+    int n; cin >> n;
+    if (n==0) return 0;
+    for (int i=0; i<n; i++) {
+      for (int j=0; j<=i; j++) {
+        cout << '*';
+      }
+      cout << endl;
+    }
+  }
+}
