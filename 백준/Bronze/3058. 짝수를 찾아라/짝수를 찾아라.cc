4d0c36a3178b4b621083108364a7864aed4f1ce0
정태양 <153086880+BitByte08@users.noreply.github.com>
2024-12-27 21:51:30 +0900

    [Bronze III] Title: 짝수를 찾아라, Time: 28 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/3058. 짝수를 찾아라/짝수를 찾아라.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() {
+    int t; cin >> t;
+    while (t--) {
+        int sum = 0, min = 101;
+        for (int i = 0; i < 7; i++){
+            int n; cin >> n;
+            if (n % 2 == 0){
+                sum += n;
+                if (n < min) min = n;
+            }
+        }
+        cout << sum << ' ' << min << '\n';
+    }
+}
