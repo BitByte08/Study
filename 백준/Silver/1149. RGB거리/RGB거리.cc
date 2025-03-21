f138f1dd4f7149672f37ef94dc79d5bef7e711f7
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-21 11:31:36 +0900

    [Silver I] Title: RGB거리, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1149. RGB거리/RGB거리.cc
+++ 백준/Silver/1149. RGB거리/RGB거리.cc
@@ -1,29 +1,20 @@
 #include <iostream>
-#include <algorithm>
 #include <vector>
 using namespace std;
-
-int main(void) {
-  int n; cin >> n;
-  vector<vector<int>> v;
-  v.push_back(vector<int>{0,0,0});
-  vector<vector<int>> dp;
-  for(int i=0;i<n;i++) {
-    vector<int> v2;
-    for(int j=0;j<3;j++) {
-      int temp; cin >> temp;
-      v2.push_back(temp);
+int main(void){
+    vector<vector<int>> v(1001, vector<int>(3,0));
+    vector<vector<int>> d(1001, vector<int>(3,0));
+    int n; cin >> n;
+    for (int i=1;i<=n;i++) {
+        for (int j=0;j<3;j++) {
+            cin >> v[i][j];
+        }
     }
-    v.push_back(v2);
-  }
-  dp.push_back(vector<int>{0,0,0});
-  for(int i=1;i<=n;i++) {
-    vector<int> v2;
-    v2.push_back(min(dp[i-1][1],dp[i-1][2])+v[i][0]);
-    v2.push_back(min(dp[i-1][0],dp[i-1][2])+v[i][1]);
-    v2.push_back(min(dp[i-1][0],dp[i-1][1])+v[i][2]);
-    dp.push_back(v2);
-  }
-  cout << min(min(dp[n][1],dp[n][2]),dp[n][0]);
-  return 0;
+    for (int i=1;i<=n;i++) {
+        d[i][0] = min(d[i-1][1],d[i-1][2]) + v[i][0];
+        d[i][1] = min(d[i-1][0],d[i-1][2]) + v[i][1];
+        d[i][2] = min(d[i-1][0],d[i-1][1]) + v[i][2];
+    }
+    cout << min(d[n][0],min(d[n][1],d[n][2])) << endl;
+
 }
