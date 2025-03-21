9dbd77c93cf9cbcbbcc1bc59b20e58e1f6b33250
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-21 15:23:04 +0900

    [Bronze III] Title: 팰린드롬인지 확인하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/10988. 팰린드롬인지 확인하기/팰린드롬인지 확인하기.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+    string s; cin >> s;
+    for (int i=0;i<s.length()/2;i++) {
+        if (s[i]!=s[s.length()-1-i]) {
+            cout << 0;
+            return 0;
+        }
+    }
+    cout << 1;
+}
