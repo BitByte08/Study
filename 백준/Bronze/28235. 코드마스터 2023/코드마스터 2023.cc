e0d05389d9a30deb00773aa261987c64920c19cc
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-22 14:37:05 +0900

    [Bronze V] Title: 코드마스터 2023, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/28235. 코드마스터 2023/코드마스터 2023.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    string s; cin >> s;
+    if(s=="SONGDO") cout << "HIGHSCHOOL";
+    if(s=="CODE") cout << "MASTER";
+    if(s=="2023") cout << "0611";
+    if(s=="ALGORITHM") cout << "CONTEST";
+    return 0;
+}
