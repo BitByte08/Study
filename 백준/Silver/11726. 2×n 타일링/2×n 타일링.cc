8c613eecdca096ce60bcca841defb9227f9082f2
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-24 11:40:47 +0900

    [Silver III] Title: 2×n 타일링, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
+++ 백준/Silver/11726. 2×n 타일링/2×n 타일링.cc
@@ -1,18 +1,13 @@
 #include <iostream>
 #include <vector>
-
+#define MOD 10007
 using namespace std;
-
-int main(void){
+int main(void) {
+    vector<int> v{0,1,2,3,5};
     int n; cin >> n;
-    vector<int> dp;
-    dp.push_back(0);
-    dp.push_back(1);
-    dp.push_back(2);
-    for(int i=3;i<=n;i++){
-        dp.push_back((dp[i-1]+dp[i-2])%10007);
+    for (int i=5;i<=n;i++) {
+        v.push_back((v[i-1]+v[i-2])%MOD);
     }
-    cout << dp[n];
+    cout << v[n] << endl;
     return 0;
 }
-
