a1cdfb8f5b353ac4fbead2dcff0123c599902993
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-25 23:42:10 +0900

    [Gold V] Title: 돌 게임 6, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9660. 돌 게임 6/돌 게임 6.cc
+++ 백준/Gold/9660. 돌 게임 6/돌 게임 6.cc
@@ -1,18 +1,12 @@
 #include <iostream>
-
+#include <vector>
 using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    long long int n;
-    cin >> n;
-
-    if(n%7==0||n%7==2){
+int main(void) {
+    long int n; cin >> n;
+    if (n%7==0||n%7==2) {
         cout << "CY";
-    }else cout << "SK";
-
+    }else {
+        cout << "SK";
+    }
     return 0;
 }
