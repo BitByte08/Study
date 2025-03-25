3c6aa628023836f29d397d574e5536bd2a307989
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-25 23:32:11 +0900

    [Silver II] Title: 돌 게임 4, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
+++ 백준/Silver/9658. 돌 게임 4/돌 게임 4.cc
@@ -1,27 +1,12 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    int n;
-    cin >> n;
-    vector<int> dp;
-    dp.push_back(0);
-    dp.push_back(0);
-    dp.push_back(1);
-    dp.push_back(0);
-    dp.push_back(1);
-    dp.push_back(1);
-    for(int i=6;i<=n;i++){
-        if(dp[i-1]==1&&dp[i-3]==1&&dp[i-4]==1){
-            dp.push_back(0);
-        }else dp.push_back(1);
+int main(void) {
+    vector<int> dp{0,0,1,0,1};
+    int n; cin >> n;
+    for (int i=5;i<=n;i++) {
+        dp.push_back(dp[i-1]+dp[i-3]+dp[i-4]!=3);
     }
-    if (dp[n] == 1) cout << "SK\n";
-    else cout << "CY\n";
+    cout << (dp[n]?"SK":"CY");
     return 0;
 }
