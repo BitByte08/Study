7938bc7bf4fcf80d805f916a431cb7e387305edf
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-25 23:26:16 +0900

    [Silver III] Title: 돌 게임 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/9657. 돌 게임 3/돌 게임 3.cc
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+int main(void) {
+    vector<int> dp{0,1,0,1,1};
+    int n; cin >> n;
+    for (int i=5;i<=n;i++) {
+        dp.push_back(dp[i-1]+dp[i-3]+dp[i-4]!=3);
+    }
+    cout << (dp[n]?"SK":"CY");
+    return 0;
+}
