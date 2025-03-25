2dba03684bfc7c8d1153fda572c89aa0e368d948
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-25 23:36:50 +0900

    [Silver III] Title: 돌 게임 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.cc
@@ -1,15 +1,11 @@
 #include <iostream>
-#include <vector>
 using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-
-    long long int n;
-    cin >> n;
-    if (n%2==1) cout << "SK\n";
-    else cout << "CY\n";
+int main(void) {
+    long int n; cin >> n;
+    if ((n&1)==0) {
+        cout << "CY";
+    }else {
+        cout << "SK";
+    }
     return 0;
 }
