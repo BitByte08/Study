84bba13fb6d32353ec4acf2e5ed7b124593b20ef
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-25 23:52:19 +0900

    [Gold II] Title: 돌 게임 7, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
+++ 백준/Gold/9661. 돌 게임 7/돌 게임 7.cc
@@ -1,16 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    long long int n;
-    cin >> n;
-    if(n%5==0||n%5==2){
+int main(void) {
+    long int n; cin >> n;
+    if (n%5==0 || n%5==2) {
         cout << "CY";
-    }else cout << "SK";
-
+    }else {
+        cout << "SK";
+    }
     return 0;
 }
