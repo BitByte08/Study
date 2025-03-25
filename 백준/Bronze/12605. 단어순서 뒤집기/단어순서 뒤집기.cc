5b5a88abbfb5b9a5adfb224680c079106768bf30
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-26 00:10:48 +0900

    [Bronze II] Title: 단어순서 뒤집기, Time: 0 ms, Memory: 2028 KB -BaekjoonHub

--- 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.cc
+++ 백준/Bronze/12605. 단어순서 뒤집기/단어순서 뒤집기.cc
@@ -1,22 +1,28 @@
 #include <iostream>
 #include <stack>
-#include <sstream>
 using namespace std;
 int main(void) {
-  int n; cin >> n;
-  stack<string> st;
-  getchar();
-  for (int i=1;i<=n;i++) {
-    string s,temp; getline(cin, s);
-    stringstream ss(s);
-    while (ss>>temp) {
-      st.push(temp);
+    int n; cin >> n;
+    getchar();
+    for (int i=0;i<n;i++) {
+        stack<string> s;
+        string str; getline(cin, str);
+        string temp = "";
+        for (char i: str) {
+            if (i!=' ') {
+                temp += i;
+            }else {
+                s.push(temp);
+                temp = "";
+            }
+        }
+        s.push(temp);
+        cout << "Case #" << i+1 << ":";
+        while (!s.empty()) {
+            cout << ' ' << s.top();
+            s.pop();
+        }
+        cout << endl;
     }
-    cout << "Case #" << i << ": ";
-    while (!st.empty()) {
-      cout << st.top() << ' '; st.pop();
-    }
-    cout << '\n';
-  }
-  return 0;
+    return 0;
 }
