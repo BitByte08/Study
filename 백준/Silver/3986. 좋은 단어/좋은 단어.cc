b9ea0871d0a466263eb22016def1b168bf3cfe60
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-03-26 08:16:34 +0900

    [Silver IV] Title: 좋은 단어, Time: 16 ms, Memory: 2268 KB -BaekjoonHub

--- 백준/Silver/3986. 좋은 단어/좋은 단어.cc
+++ 백준/Silver/3986. 좋은 단어/좋은 단어.cc
@@ -2,20 +2,22 @@
 #include <stack>
 using namespace std;
 int main(void) {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
-  int n; string s; cin >> n;
-  int res = 0;
-  while(n--) {
-    cin >> s;
-    stack<char> st;
-    for(auto c : s) {
-      if(st.empty()) st.push(c);
-      else if(st.top()!=c) st.push(c);
-      else if(st.top()==c) st.pop();
+    int n,sum=0; cin >> n;
+    while (n--) {
+        stack<char> st;
+        string s; cin >> s;
+        for (auto c:s) {
+            if (st.empty()) st.push(c);
+            else {
+                if (st.top()==c) {
+                    st.pop();
+                }else {
+                    st.push(c);
+                }
+            }
+        }
+        sum+=st.empty();
     }
-    if(st.empty()) res++;
-  }
-  cout << res;
-  return 0;
+    cout << sum;
+    return 0;
 }
