5123c12697106f7f8c3df7e5924d29b009e3df1c
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-03 21:51:32 +0900

    [Gold V] Title: 빗물, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Gold/14719. 빗물/빗물.cc
+++ 백준/Gold/14719. 빗물/빗물.cc
@@ -1,16 +1,21 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(void){
-  int res = 0, h, w; cin >> h >> w;
-  vector<int> v(w);
-  for (int i=0;i<w;i++) cin >> v[i];
-  for (int i=1;i<w-1;i++) {
-    int l, r; l=r=0;
-    for (int j=0;j<i;j++) l = max(l, v[j]);
-    for (int j=i;j<w;j++) r = max(r, v[j]);
-    res += max(0, min(l, r) - v[i]);
-  }
-  cout << res;
-  return 0;
+int main(void) {
+    int h,w; cin >> h >> w;
+    int sum = 0;
+    vector<int> v(w,0);
+    for (int i=0;i<w;i++) cin >> v[i];
+    for (int i=1;i<w-1;i++) {
+        int left=0,right=0;
+        for (int j=0;j<i;j++) {
+            left = max(left,v[j]);
+        }
+        for (int j=i+1;j<w;j++) {
+            right = max(right,v[j]);
+        }
+        sum += max(min(left,right) - v[i],0);
+    }
+    cout << sum;
+    return 0;
 }
