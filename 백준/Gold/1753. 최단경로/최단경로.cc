b3bc6898090b47a72c09a3499238b2344a09f80e
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-04 09:33:22 +0900

    [Gold IV] Title: 최단경로, Time: 92 ms, Memory: 16844 KB -BaekjoonHub

--- 백준/Gold/1753. 최단경로/최단경로.cc
+++ 백준/Gold/1753. 최단경로/최단경로.cc
@@ -1,48 +1,40 @@
+#include <iostream>
 #include <vector>
 #include <queue>
-#include <iostream>
+#include <algorithm>
+
+#define INF ~(1<<31)
 using namespace std;
-# define INF 99999999
-vector<int> dijkstra(int start, int N, vector<pair<int, int> > graph[]) {
-  vector<int> dist(N+1, INF);
-  priority_queue<pair<int, int>> pq;
-  dist[start] = 0;
-  pq.push({0, start});
+vector<vector<pair<int,int>>> v(300001,vector<pair<int,int>>());
+vector<int> dijkstra(300001,INF);
+int main(void) {
+  ios::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int n,m,start; cin >> n >> m >> start;
+
+  for (int i=0;i<m;i++) {
+    int x,y,c; cin >> x >> y >> c;
+    v[x].push_back({y,c});
+  }
+  priority_queue<pair<int,int>> pq;
+  pq.push({0,start});
+  dijkstra[start] = 0;
   while (!pq.empty()) {
-    int cur_dist = -pq.top().first;
-    int cur_node = pq.top().second;
+    int node = -pq.top().first;
+    int cost = pq.top().second;
     pq.pop();
-    if(dist[cur_node] < cur_dist) {
-      continue;
-    }
-    for (int i = 0; i < graph[cur_node].size(); i++) {
-      int nxt_node = graph[cur_node][i].first;
-      int nxt_dist = cur_dist + graph[cur_node][i].second;
-      if (nxt_dist < dist[nxt_node]){
-        dist[nxt_node] = nxt_dist;
-        pq.push({-nxt_dist, nxt_node});
+    for (auto i : v[cost]) {
+      int nextNode = i.first;
+      int nextCost = i.second + node;
+      if (dijkstra[nextNode] > nextCost) {
+        dijkstra[nextNode] = nextCost;
+        pq.push({-nextCost,nextNode});
       }
     }
   }
-  return dist;
-}
-int main(void) {
-  ios_base::sync_with_stdio(false);
-  cin.tie(NULL); cout.tie(NULL);
-  int n, m; cin >> n >> m;
-  int start; cin >> start;
-  vector<pair<int, int>> graph[n+1];
-  for (int i = 0; i < m; i++) {
-    int x, y, z; cin >> x >> y >> z;
-    graph[x].push_back({y, z});
-  }
-  vector<int> dist = dijkstra(start, n, graph);
-  for(int i=1;i<=n;i++) {
-    if(dist[i]==INF) {
-      cout << "INF\n";
-    }else {
-      cout << dist[i] << '\n';
-    }
+  for (int i=1;i<=n;i++) {
+    if (dijkstra[i]==INF) cout << "INF" << '\n';
+    else cout << dijkstra[i] << '\n';
   }
   return 0;
 }
