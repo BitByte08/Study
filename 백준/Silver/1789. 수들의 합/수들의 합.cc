3d5d0056ffda4234f78261c80aeac70c843a143b
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-06 21:03:53 +0900

    [Silver V] Title: 수들의 합, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1789. 수들의 합/수들의 합.cc
+++ 백준/Silver/1789. 수들의 합/수들의 합.cc
@@ -2,16 +2,13 @@
 using namespace std;
 int main(void) {
   uint64_t n; cin >> n;
-  uint64_t sum = 0,i=1;
-  while (1) {
-    sum += i;
-    if (sum>n) {
-      i--;
-      break;
-    }
-    if (sum==n) break;
-    i++;
+  uint64_t t = 0;
+  uint64_t sum = 0,role = n-sum;
+  for (int i=1;i<=role;i++) {
+    sum+=i;
+    role=n-sum;
+    t++;
   }
-  cout << i;
+  cout << t;
   return 0;
 }
