b57f0c83b9a12b9271261097ab527e932816ebc4
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-18 17:36:29 +0900

    [Silver V] Title: 다리 놓기, Time: 12 ms, Memory: 2024 KB -BaekjoonHub

--- 백준/Silver/1010. 다리 놓기/다리 놓기.cc
+++ 백준/Silver/1010. 다리 놓기/다리 놓기.cc
@@ -1,18 +1,17 @@
 #include <iostream>
 using namespace std;
-int main(void){
-    int i,t;
-    cin >> t;
-    for(i=0;i<t;i++){
-        long long int res=1;
-        int a,b,j,k=1;
-        cin >> a >> b;
-        for(j=b;j>b-a;j--){
-            res*=j;
-            res/=k;
-            k++;
-        }
-        cout << res << '\n';
+int nCr[31][31];
+int main() {
+  for (int i=1;i<=30;i++) {
+    nCr[i][i] = nCr[i][0] = 1;
+    for (int j=1;j<i;j++) {
+      nCr[i][j] = nCr[i-1][j-1] + nCr[i-1][j];
     }
-    return 0;
+  }
+  int t; cin >> t;
+  while (t--) {
+    int r,n; cin >> r >> n;
+    cout << nCr[n][r] << '\n';
+  }
+  return 0;
 }
