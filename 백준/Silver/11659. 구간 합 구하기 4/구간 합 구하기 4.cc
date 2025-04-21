c135b5530f4e145380a34e4ebedab87607e6b986
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-21 18:55:08 +0900

    [Silver III] Title: 구간 합 구하기 4, Time: 36 ms, Memory: 2916 KB -BaekjoonHub

--- 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
+++ 백준/Silver/11659. 구간 합 구하기 4/구간 합 구하기 4.cc
@@ -1,24 +1,18 @@
 #include <iostream>
+#include <vector>
 using namespace std;
-
-int main(void){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL);
-    cout.tie(NULL);
-    int arr_size,t,a,b;
-    cin >> arr_size >> t;
-    int arr[arr_size];
-    int i;
-    arr[0]=0;
-    for(i=1;i<=arr_size;i++) {
-        cin >> arr[i];
-    }
-    for(i=2;i<=arr_size;i++) {
-        arr[i]+=arr[i-1];
-    }
-    for(i=0;i<t;i++) {
-        cin >> a >> b;
-        cout << arr[b] - arr[a-1]<< '\n';
-    }
-    return 0;
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  vector<int> v;
+  int n,t; cin >> n >> t;
+  int temp; cin >> temp; v.push_back(temp);
+  for (int i = 1; i < n; i++) {
+    cin >> temp; v.push_back(v[i-1] + temp);
+  }
+  while (t--) {
+    int a,b; cin >> a >> b;
+    cout << v[b-1] - v[a-2] << '\n';
+  }
+  return 0;
 }
