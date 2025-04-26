f43b361627b2620daeb66838a4fc5a03ede9b3aa
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-26 22:48:48 +0900

    [Bronze V] Title: Rats, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/18301. Rats/Rats.cc
+++ 백준/Bronze/18301. Rats/Rats.cc
@@ -1,10 +1,7 @@
 #include <iostream>
-
 using namespace std;
-
-int main() {
-    int n1,n2,n12;
-    cin >> n1 >> n2 >> n12;
-    cout << ((n1 + 1)*(n2 + 1)/(n12 + 1) - 1);
+int main(void){
+    int n1, n2, n3; cin >> n1 >> n2 >> n3;
+    cout << (n1+1)*(n2+1)/(n3+1)-1;
     return 0;
 }
