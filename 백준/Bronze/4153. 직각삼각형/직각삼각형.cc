c265b988bf0690c3bfcaefe8e5e01f608db699d9
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-27 21:24:36 +0900

    [Bronze III] Title: 직각삼각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.cc
@@ -1,27 +1,13 @@
 #include <iostream>
-
 using namespace std;
-
 int main(void){
-    int a,b,c;
-    while(1){
-        cin >> a >> b >> c;
-        if(a==b&&b==c&&c==a&&a==0){
-            break;
-        }
-        if (a > b) {
-            int temp = a;
-            a = b;
-            b = temp;
-        }
-        if (b > c) {
-            int temp = c;
-            c = b;
-            b = temp;
-        }
-        cout << ((a*a)+(b*b)==(c*c)?"right":"wrong");
-        cout << '\n';
+    while(true){
+        int a,b,c; cin >> a >> b >> c;
+        if(!a&&!b&&!c) return 0;
+        a *= a;
+        b *= b;
+        c *= c;
+        int temp = a+b+c - max(a,max(b,c));
+        cout << (temp==max(a,max(b,c))?"right":"wrong") << '\n';
     }
-
-    return 0;
 }
