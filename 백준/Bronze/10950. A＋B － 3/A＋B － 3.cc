82d009a20eff1a752255f8477aededd1381c6f6a
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-27 20:44:31 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+int main(void){
+    int t; cin >> t;
+    while(t--){
+        int a,b; cin >> a >> b;
+        cout << a+b << '\n';
+    }
+    return 0;
+}
