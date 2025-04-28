7df94542062ce55556d962b660e7c9de6036bc19
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-04-28 14:14:06 +0900

    [Gold II] Title: 가장 긴 증가하는 부분 수열 3, Time: 576 ms, Memory: 12084 KB -BaekjoonHub

---
+++ 백준/Gold/12738. 가장 긴 증가하는 부분 수열 3/가장 긴 증가하는 부분 수열 3.cc
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int n;cin >> n;
+    vector<int> arr(n);
+    vector<int> dp;
+    for(int i = 0; i < n; i++) cin >> arr[i];
+    for(int i = 0; i < n; i++) {
+        int num = arr[i];
+        auto temp = lower_bound(dp.begin(), dp.end(), num);
+        if(temp == dp.end()) dp.push_back(num);
+        else *temp = num;
+    }
+
+    cout << dp.size() << endl;
+    return 0;
+}
