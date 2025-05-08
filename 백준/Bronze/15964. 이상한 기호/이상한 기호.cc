99e0fc4052b94e65e059166eefd2ed9bd660125a
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-05-08 23:38:50 +0900

    [Bronze V] Title: 이상한 기호, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.cc
@@ -1,10 +1,7 @@
 #include <iostream>
-
 using namespace std;
-
 int main(void){
-    long long int a,b;
-    cin >> a >> b;
-    cout << (a+b)*(a-b);
+    long long A,B; cin >> A >> B;
+    cout << (A+B)*(A-B);
     return 0;
 }
