207c6f34e705fc33c7fa8151349bb08f4f9f5355
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-05-13 21:40:20 +0900

    [Bronze IV] Title: 평균 점수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10039. 평균 점수/평균 점수.cc
+++ 백준/Bronze/10039. 평균 점수/평균 점수.cc
@@ -1,14 +1,12 @@
 #include <iostream>
-
 using namespace std;
-
-int main(void) {
-    int i,sum=0;
-    for(i=0;i<5;i++) {
-        int temp;
-        cin >> temp;
-        sum+=(temp>40?temp:40);
-    }
-    cout << sum/5;
+int main(void){
+    int a,b,c,d,e; cin >> a >> b >> c >> d >> e;
+    a=max(40,a);
+    b=max(40,b);
+    c=max(40,c);
+    d=max(40,d);
+    e=max(40,e);
+    cout << (a+b+c+d+e)/5;
     return 0;
 }
