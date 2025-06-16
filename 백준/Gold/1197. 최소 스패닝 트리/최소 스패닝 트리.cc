378e1e4dbaead19920156c3865255eefda2d479e
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-06-16 19:33:57 +0900

    [Gold IV] Title: 최소 스패닝 트리, Time: 36 ms, Memory: 4496 KB -BaekjoonHub

--- 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
+++ 백준/Gold/1197. 최소 스패닝 트리/최소 스패닝 트리.cc
@@ -1,44 +1,40 @@
 #include <iostream>
 #include <vector>
 #include <algorithm>
-#define PAIR pair<int,pair<int, int>>
+#define Pair pair<int,pair<int,int>>
 using namespace std;
-
-
-vector<int> parent;
-int union_find(int n){
-    if(parent[n]==n) return n;
-    return parent[n] = union_find(parent[n]);
+vector<int> v;
+int find_parent(int x) {
+  if (v[x]!=x) v[x]=find_parent(v[x]);
+  return v[x];
 }
-void union_root(int x, int y){
-    x = union_find(x);
-    y = union_find(y);
-    if(x!=y) parent[y] = x;
+void union_parent(int x,int y) {
+  x = find_parent(x);
+  y = find_parent(y);
+  if (x<y) v[y]=x;
+  else v[x]=y;
 }
-int main(void){
-    ios_base::sync_with_stdio(false);
-    cin.tie(NULL); cout.tie(NULL);
-    vector<PAIR> e_list;
-    vector<PAIR> res;
-    int v,e; cin >> v >> e;
-    int re = 0;
-    parent.resize(v+1,0);
-    for(int i=1;i<=v;i++) parent[i] = i;
-    for(int i=0;i<e;i++){
-        int a,b,c; cin >> a >> b >> c;
-        e_list.push_back({c,{a,b}});
+int main(void) {
+  ios_base::sync_with_stdio(false);
+  cin.tie(NULL); cout.tie(NULL);
+  int n,m; cin >> n >> m;
+  vector<Pair> edges;
+  v.resize(n+1);
+  for (int i=1;i<=n;i++) v[i]=i;
+  for (int i=0;i<m;i++) {
+    int a,b,cost; cin >> a >> b >> cost;
+    edges.push_back({cost,{a,b}});
+  }
+  int res = 0;
+  sort(edges.begin(),edges.end());
+  for (Pair edge: edges) {
+    pair<int,int> cost = edge.second;
+    if (find_parent(cost.first)!=find_parent(cost.second)) {
+      union_parent(cost.first,cost.second);
+      res+=edge.first;
     }
-    sort(e_list.begin(),e_list.end());
-    for(PAIR p: e_list){
-        int a = p.second.first;
-        int b = p.second.second;
-
-        if(union_find(a)==union_find(b)) continue;
-        res.push_back(p);
-        re += p.first;
-        union_root(a,b);
-        if(res.size() == v-1) break;
-    }
-    cout << re;
-    return 0;
+  }
+  cout << res;
+  return 0;
 }
+
