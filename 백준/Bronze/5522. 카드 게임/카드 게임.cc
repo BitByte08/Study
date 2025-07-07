5688b0ba3090d51a2e3c869e04802e5e8146c061
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-07-07 16:04:38 +0900

    [Bronze V] Title: 카드 게임, Time: 0 ms, Memory: 2020 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/5522. 카드 게임/카드 게임.cc
+++ 백준/Bronze/5522. 카드 게임/카드 게임.cc
@@ -1,14 +1,11 @@
 #include <iostream>
-
 using namespace std;
-
 int main(void){
-    long long int t,cnt=0;
-    int i;
-    for(i=0;i<5;i++){
-        cin >> t;
-        cnt+=t;
+    uint64_t sum = 0;
+    int t=5; while(t--){
+        uint64_t temp; cin >> temp;
+        sum+=temp;
     }
-    cout <<cnt;
+    cout << sum;
     return 0;
 }
