56a3e742fb50517c8457cb7c2425640c514e833b
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-07-10 22:21:51 +0900

    [Silver II] Title: 랜선 자르기, Time: 4 ms, Memory: 2220 KB -BaekjoonHub

--- 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
+++ 백준/Silver/1654. 랜선 자르기/랜선 자르기.cc
@@ -2,6 +2,7 @@
 #include <vector>
 using namespace std;
 int main(void) {
+    ios_base::sync_with_stdio(false); cin.tie(0); cout.tie(0);
     __uint64_t left, right, mid, ans; left=right=mid=ans=0;
     int32_t k,n; cin >> k >> n;
     vector<__uint64_t> v(k);
