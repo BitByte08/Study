87c536b23bb4fa2d45f9f5b0431a0d58c75c55f0
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-07-16 09:42:41 +0900

    [Silver III] Title: N과 M (3), Time: 420 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <vector>
+using namespace std;
+vector<int> ans;
+vector<int> used(9, false);
+void backtracking(int start, int max, int bound);
+int main(void) {
+    int n, m; cin >> n >> m;
+    backtracking(1,n,m);
+    return 0;
+}
+void backtracking(int start, int max, int bound) {
+    if (ans.size() == bound) {
+        for (int i : ans) cout << i << " ";
+        cout <<'\n';
+        return;
+    }
+    for (int i=1;i<=max;i++) {
+        ans.push_back(i);
+        backtracking(i+1,max,bound);
+        ans.pop_back();
+    }
+}
