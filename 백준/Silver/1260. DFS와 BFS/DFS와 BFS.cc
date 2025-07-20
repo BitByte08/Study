25b709ac882d4a417e1a628c0623e8ee7a250106
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-07-20 22:26:31 +0900

    [Silver II] Title: DFS와 BFS, Time: 4 ms, Memory: 2160 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.cc
@@ -1,57 +1,50 @@
 #include <algorithm>
 #include <iostream>
-#include <vector>
 #include <queue>
 #include <stack>
+#include <vector>
 using namespace std;
-
-vector<vector<int>> v(1001,vector<int>(0));
-int n, s;
-
-vector<int> dfs();
-vector<int> bfs();
-
-int main() {
-    int t; cin >> n >> t >> s;
-    for (int i=0;i<t;i++) {
+int main(void) {
+    int n, m; cin >> n >> m;
+    int start; cin >> start;
+    vector<vector<int>> v(n+1);
+    for (int i=0;i<m;i++) {
         int a,b; cin >> a >> b;
         v[a].push_back(b);
         v[b].push_back(a);
     }
-    for (auto i : dfs()) cout << i << ' ';
-    cout << endl;
-    for (auto i : bfs()) cout << i << ' ';
-}
-
-vector<int> dfs() {
-    vector<int> ans;
-    vector<int> visited(1001,0);
-    stack<int> st;
-    st.push(s);
-    while (!st.empty()) {
-        int cur = st.top(); st.pop();
-        if (!visited[cur]) {
-            visited[cur] = 1;
-            ans.push_back(cur);
-            sort(v[cur].begin(), v[cur].end(), greater<int>());
-            for (auto i : v[cur]) if (!visited[i]) st.push(i);
+    vector<int> visited1(n+1, false);
+    stack<int> s;
+    vector<int> ans1;
+    s.push(start);
+    while (!s.empty()) {
+        int now = s.top(); s.pop();
+        if (!visited1[now]) {
+            visited1[now] = true;
+            ans1.push_back(now);
+            sort(v[now].begin(), v[now].end(), greater<int>());
+            for (auto i : v[now]) {
+                if (!visited1[i]) s.push(i);
+            }
         }
     }
-    return ans;
-}
-vector<int> bfs() {
-    vector<int> ans;
-    vector<int> visited(1001,0);
+    for (auto x : ans1) cout << x << " ";
+    cout << '\n';
+    vector<int> visited2(n+1,false);
     queue<int> q;
-    q.push(s);
+    vector<int> ans2;
+    q.push(start);
     while (!q.empty()) {
-        int cur = q.front(); q.pop();
-        if (!visited[cur]) {
-            visited[cur] = 1;
-            ans.push_back(cur);
-            sort(v[cur].begin(), v[cur].end());
-            for (auto i : v[cur]) if (!visited[i]) q.push(i);
+        int now = q.front(); q.pop();
+        if (!visited2[now]) {
+            visited2[now] = true;
+            ans2.push_back(now);
+            sort(v[now].begin(), v[now].end());
+            for (auto i : v[now]) {
+                if (!visited2[i]) q.push(i);
+            }
         }
     }
-    return ans;
+    for (auto x : ans2) cout << x << " ";
+    return 0;
 }
