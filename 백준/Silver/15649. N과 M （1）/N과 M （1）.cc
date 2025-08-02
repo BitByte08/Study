bd7237b8446e7c7d6f65bc08a5552dafada36515
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-08-03 01:55:15 +0900

    [Silver III] Title: N과 M (1), Time: 32 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.cc
@@ -1,27 +1,26 @@
-#include <cstring>
 #include <iostream>
 #include <vector>
 using namespace std;
-vector<int> ans;
-vector<int> used;
-void backtracking(int start, int max, int bound);
+void backtrack(int level);
+int n, m;
 int main(void) {
-    int n, m; cin >> n >> m;
-    used.resize(n+1, false);
-    backtracking(1, n, m);
+    cin >> n >> m;
+    backtrack(0);
 }
-void backtracking(int start, int max, int bound) {
-    if (ans.size() == bound) {
-        for (int i : ans) cout << i << ' ';
+void backtrack(int level) {
+    static vector<int> used(n+1,0);
+    static vector<int> ans(m,0);
+    if (m == level) {
+        for (auto i : ans) cout << i << " ";
         cout << '\n';
         return;
     }
-    for (int i = 1; i <= max; i++) {
-        if (used[i]) continue;
-        used[i] = true;
-        ans.push_back(i);
-        backtracking(i, max, bound);
-        ans.pop_back();
-        used[i] = false;
+    for (int i=0;i<n;i++) {
+        if (!used[i]) {
+            used[i] = 1;
+            ans[level] = i+1;
+            backtrack(level+1);
+            used[i] = 0;
+        }
     }
 }
