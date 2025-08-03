d04ac2764f9fd0b8f7e507274c356297ad02eebb
정태양 <153086880+BitByte08@users.noreply.github.com>
2025-08-04 02:30:43 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.cc
@@ -1,12 +1,10 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-int main(void){
-    vector<int> dp{0,1,3};
+int main(void) {
+    vector<int> v = {0,1,3};
     int n; cin >> n;
-    for(int i=3;i<=n;i++){
-        dp.push_back((dp[i-1]+(dp[i-2]*2))%10007);
-    }
-    cout << dp[n];
+    for (int i=3;i<=n;i++) v.push_back((v[i-1]+v[i-2]*2)%10007);
+    cout << v[n];
     return 0;
 }
